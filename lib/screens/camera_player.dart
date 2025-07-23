25698ed6ac7bac1305ed0aa660ade041f086940f
junCook <55830905+MrTuyennn@users.noreply.github.com>
2025-07-23 21:10:38 +0700

    feat: Implement feature hide/show mic or sound && switch mic && sound with camera type

--- lib/screens/camera_player.dart
+++ lib/screens/camera_player.dart
@@ -14,38 +14,44 @@ class _CameraPlayerState extends State<CameraPlayer> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            const Text(
-              'Camera Multi',
-              style: TextStyle(decoration: TextDecoration.none, color: Colors.white, fontSize: 14),
-            ),
-            Expanded(
-                child: defaultTargetPlatform == TargetPlatform.android
-                    ? AndroidView(
-                        viewType: ModuleChannel.viewPlayer,
-                        creationParams: const {
-                          "uuid": "H23R9K94ZULVDVCF111A",
-                          "pass": "Js2nvQ",
-                          "height": 0,
-                          "width": 0
-                        },
-                        layoutDirection: TextDirection.ltr,
-                        creationParamsCodec: const StandardMessageCodec(),
-                      )
-                    : UiKitView(
-                        viewType: ModuleChannel.viewPlayer,
-                        creationParams: const {
-                          "uuid": "H23R9K94ZULVDVCF111A",
-                          "pass": "Js2nvQ",
-                          "height": 500,
-                          "width": 1000
-                        },
-                        layoutDirection: TextDirection.ltr,
-                        creationParamsCodec: const StandardMessageCodec(),
-                      ))
-          ],
+      color: Colors.white,
+      child: Column(
+        children: [
+          const Text(
+            'Camera Multi',
+            style: TextStyle(decoration: TextDecoration.none, color: Colors.white, fontSize: 14),
+          ),
+          Expanded(
+              child: defaultTargetPlatform == TargetPlatform.android
+                  ? AndroidView(
+                      viewType: ModuleChannel.viewPlayer,
+                      creationParams: const {
+                        "uuid": "H23R9K94ZULVDVCF111A",
+                        "pass": "Js2nvQ",
+                        "height": 0,
+                        "width": 0,
+                        "type": 0,
+                        "mic": 1,
+                        "sound": 1
+                      },
+                      layoutDirection: TextDirection.ltr,
+                      creationParamsCodec: const StandardMessageCodec(),
+                    )
+                  : UiKitView(
+                      viewType: ModuleChannel.viewPlayer,
+                      creationParams: const {
+                        "uuid": "H23R9K94ZULVDVCF111A",
+                        "pass": "Js2nvQ",
+                        "height": 500,
+                        "width": 1000,
+                        "type": 0,
+                        "mic": 0,
+                        "sound": 0
+                      },
+                      layoutDirection: TextDirection.ltr,
+                      creationParamsCodec: const StandardMessageCodec(),
+                    ))
+        ],
       ),
     );
   }
